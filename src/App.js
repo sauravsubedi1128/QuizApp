5c4c862d6cdea88f8ec3274d2110fc7376d5864f
Saurav Subedi <sauravsubedi1128@gmail.com>
2023-11-17 15:20:31 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,122 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [showFinalResult, setFinalResult] = useState(false);
+  const [score, setScore] = useState(0);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const questions = [
+    {
+      text: "What is the capital of Nepal?",
+      options: [
+        { id: 0, text: "Jhapa", isCorrect: false },
+        { id: 1, text: "Biratnagar", isCorrect: false },
+        { id: 2, text: "Pokhara", isCorrect: false },
+        { id: 3, text: "Kathmandu", isCorrect: true },
+      ],
+    },
+    {
+      text: "When was constitution of Nepal promulgated?",
+      options: [
+        { id: 0, text: "2015", isCorrect: true },
+        { id: 1, text: "1776", isCorrect: false },
+        { id: 2, text: "1774", isCorrect: false },
+        { id: 3, text: "1826", isCorrect: false },
+      ],
+    },
+    {
+      text: "Who was the first president of the Nepal?",
+      options: [
+        { id: 0, text: "Ram Baran Yadav", isCorrect: true },
+        { id: 1, text: "Bidya Devi Bhandari", isCorrect: false },
+        { id: 2, text: "Jhalanath Khanal", isCorrect: false },
+        { id: 3, text: "Ram Chandra Poudel", isCorrect: false },
+      ],
+    },
+    {
+      text: "What is the largest river in the Nepal?",
+      options: [
+        { id: 0, text: "Seti", isCorrect: false },
+        { id: 1, text: "Koshi", isCorrect: true },
+        { id: 2, text: "Karnali", isCorrect: false },
+        { id: 3, text: "Bheri", isCorrect: false },
+      ],
+    },
+    {
+      text: "Which of the following countries DO NOT border the US?",
+      options: [
+        { id: 0, text: "India", isCorrect: false },
+        { id: 1, text: "Bhutan", isCorrect: true },
+        { id: 2, text: "China", isCorrect: true },
+        { id: 3, text: "Tibet", isCorrect: false },
+      ],
+    },
+  ];
+
+  const optionClicked = (isCorrect) => {
+    if (isCorrect) {
+      setScore(score + 1);
+    }
+    if (currentQuestion + 1 < questions.length) {
+      setCurrentQuestion(currentQuestion + 1);
+    } else {
+      setFinalResult(true);
+    }
+  };
+  const reStartGame = () => {
+    setScore(0);
+    setCurrentQuestion(0);
+    setFinalResult(false);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Header */}
+      <h1 className="font-bold">Quiz</h1>
+
+      {/* Current Score */}
+      <h2 className="font-bold">Current Score: {score}</h2>
+
+      {showFinalResult ? (
+        /* final result */
+        <div className="m-auto bg-gray-400 p-4 text-white border-2 rounded-xl h-auto w-[50%]">
+          <h1>Final result</h1>
+          <h2>
+            {score} out of {questions.length} correct - (
+            {(score / questions.length) * 100}%){" "}
+          </h2>
+          <button
+            onClick={() => reStartGame()}
+            className="bg-red-600 border-2 rounded-xl p-2 m-2"
+          >
+            Restart Game
+          </button>
+        </div>
+      ) : (
+        /* Question card */
+        <div className="questionCard bg-gray-500 p-6 w-[80%] h-auto m-auto rounded-xl font-bold text-white">
+          <h2>
+            {currentQuestion + 1} out of {questions.length}
+          </h2>
+          <h3 className="text-blue-800 font-bold">
+            {questions[currentQuestion].text}
+          </h3>
+          <div className="">
+            <ul>
+              {questions[currentQuestion].options.map((option) => (
+                <li
+                  className="border-2 m-2 rounded-xl bg-gray-400"
+                  key={option.id}
+                  onClick={() => optionClicked(option.isCorrect)}
+                >
+                  {" "}
+                  {option.text}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/que.js
@@ -0,0 +1,185 @@
+const questions = [
+  {
+    "response_code": 0,
+    "results": [
+      {
+        "type": "multiple",
+        "difficulty": "medium",
+        "category": "General Knowledge",
+        "question": "Which Italian automobile manufacturer gained majority control of U.S. automobile manufacturer Chrysler in 2011?",
+        "correct_answer": "Fiat",
+        "incorrect_answers": [
+          "Maserati",
+          "Alfa Romeo",
+          "Ferrari"
+        ]
+      },
+      {
+        "type": "multiple",
+        "difficulty": "easy",
+        "category": "Entertainment: Japanese Anime &amp; Manga",
+        "question": "Who is the colossal titan in &quot;Attack On Titan&quot;?",
+        "correct_answer": "Bertolt Hoover",
+        "incorrect_answers": [
+          "Reiner",
+          "Eren",
+          "Sasha"
+        ]
+      },
+      {
+        "type": "multiple",
+        "difficulty": "hard",
+        "category": "Entertainment: Video Games",
+        "question": "Which of these characters in &quot;Undertale&quot; can the player NOT go on a date with?",
+        "correct_answer": "Toriel",
+        "incorrect_answers": [
+          "Papyrus",
+          "Undyne",
+          "Alphys"
+        ]
+      },
+      {
+        "type": "multiple",
+        "difficulty": "medium",
+        "category": "Entertainment: Video Games",
+        "question": "In the &quot;Pikmin&quot; series, what is the only pikmin type to possess visible ears?",
+        "correct_answer": "Yellow",
+        "incorrect_answers": [
+          "Red",
+          "White",
+          "Winged"
+        ]
+      },
+      {
+        "type": "boolean",
+        "difficulty": "medium",
+        "category": "Science &amp; Nature",
+        "question": "The Doppler effect applies to light.",
+        "correct_answer": "True",
+        "incorrect_answers": [
+          "False"
+        ]
+      },
+      {
+        "type": "multiple",
+        "difficulty": "hard",
+        "category": "Entertainment: Music",
+        "question": "Pamina and Papageno are characters in what Mozart opera?",
+        "correct_answer": "The Magic Flute",
+        "incorrect_answers": [
+          "The Marriage of Figaro",
+          "The Impresario",
+          "The Goose of Cairo"
+        ]
+      },
+      {
+        "type": "multiple",
+        "difficulty": "medium",
+        "category": "Entertainment: Video Games",
+        "question": "Which of the following is NOT an official game in Nintendo&#039;s Super Smash Bros. series?",
+        "correct_answer": "Super Smash Bros. Crusade",
+        "incorrect_answers": [
+          "Super Smash Bros. Melee",
+          "Super Smash Bros. Brawl",
+          "Super Smash Bros. for Nintendo 3DS and Wii U"
+        ]
+      },
+      {
+        "type": "multiple",
+        "difficulty": "medium",
+        "category": "Entertainment: Musicals &amp; Theatres",
+        "question": "Which of the following is not a piece from the 1950&#039;s musical West Side Story?",
+        "correct_answer": "The Back Alley",
+        "incorrect_answers": [
+          "Maria",
+          "Tonight",
+          "Mambo"
+        ]
+      },
+      {
+        "type": "multiple",
+        "difficulty": "medium",
+        "category": "History",
+        "question": "Which Louis was known as &#039;The Sun King of France&#039;?",
+        "correct_answer": "Louis XIV",
+        "incorrect_answers": [
+          "Louis XIII",
+          "Louis XV",
+          "Louis XVI"
+        ]
+      },
+      {
+        "type": "multiple",
+        "difficulty": "easy",
+        "category": "Science: Computers",
+        "question": "This mobile OS held the largest market share in 2012.",
+        "correct_answer": "iOS",
+        "incorrect_answers": [
+          "Android",
+          "BlackBerry",
+          "Symbian"
+        ]
+      },
+      {
+        "type": "multiple",
+        "difficulty": "hard",
+        "category": "History",
+        "question": "What was the original name of New York City?",
+        "correct_answer": "New Amsterdam",
+        "incorrect_answers": [
+          "New London",
+          "New Paris",
+          "New Rome"
+        ]
+      },
+      {
+        "type": "multiple",
+        "difficulty": "medium",
+        "category": "Entertainment: Film",
+        "question": "Mark Wahlberg played the titular character of which 2008 video-game adaptation?",
+        "correct_answer": "Max Payne",
+        "incorrect_answers": [
+          "Alan Wake",
+          "Hitman",
+          "God Of War"
+        ]
+      },
+      {
+        "type": "multiple",
+        "difficulty": "medium",
+        "category": "Entertainment: Video Games",
+        "question": "Which of the following Terran units from the RTS game Starcraft was first introduced in the expansion Brood War?",
+        "correct_answer": "Medic",
+        "incorrect_answers": [
+          "Wraith",
+          "Science Vessel",
+          "SCV"
+        ]
+      },
+      {
+        "type": "multiple",
+        "difficulty": "medium",
+        "category": "Science: Computers",
+        "question": "The teapot often seen in many 3D modeling applications is called what?",
+        "correct_answer": "Utah Teapot",
+        "incorrect_answers": [
+          "Pixar Teapot",
+          "3D Teapot",
+          "Tennessee Teapot"
+        ]
+      },
+      {
+        "type": "multiple",
+        "difficulty": "hard",
+        "category": "Mythology",
+        "question": "What was the punishment for Sysiphus&#039;s craftiness?",
+        "correct_answer": "Cursed to roll a boulder up a hill for eternity.",
+        "incorrect_answers": [
+          "Tied to a boulder for eternity, being pecked by birds.",
+          "Standing in a lake filled with water he could not drink.",
+          "To fell a tree that regenerated after every axe swing."
+        ]
+      }
+    ]
+  }
+]
